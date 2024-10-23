080365c7590029449123d31a1d85ef9408e89720
virag-ky <vkormoczy@gmail.com>
2024-10-23 14:06:46 +0300

    Reacting to events

--- section3/src/components/TabButton.jsx
+++ section3/src/components/TabButton.jsx
@@ -1,7 +1,11 @@
 function TabButton({ children }) {
+  function handleClick() {
+    console.log("Hello World");
+  }
+
   return (
     <li>
-      <button>{children}</button>
+      <button onClick={handleClick}>{children}</button>
     </li>
   );
 }
